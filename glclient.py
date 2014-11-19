3c53d3c520954fb8a543651e90a3429b01095987
nanotone <nanotone@gmail.com>
2014-11-19 00:27:30 -0500

    Double the visualizations, double the fun

--- glclient.py
+++ glclient.py
@@ -19,67 +19,113 @@ def rgb_from_hexcolor(hexcolor):
     return (int(hexcolor[1:3], 16) / 255.0, int(hexcolor[3:5], 16) / 255.0, int(hexcolor[5:7], 16) / 255.0)
 RGB_COLORS = map(rgb_from_hexcolor, HEXCOLORS)
 
-def init_gl(width, height):
-    glMatrixMode(GL_PROJECTION)  # set viewing projection
-    glLoadIdentity()
-    #ratio = 1.0  #float(width) / height
-    glOrtho(0.0, 1.0, -1.0, 1.0, 1.0, -1.0)
-    glMatrixMode(GL_MODELVIEW)  # return to position viewer
-
 engine_lock = threading.Lock()
 
 class Renderer(object):
-    def __init__(self):
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
         self.last_update = 0
         self.top_2nd_note_weight = 0.3
+        self.render_frame = lambda: None
 
-    def render_frame(self):
+    def set_keyboard_viz(self):
+        self.viz = 'keyboard'
+        self.render_frame = self.render_keyboard
+        glMatrixMode(GL_PROJECTION)  # set viewing projection
+        glLoadIdentity()
+        glOrtho(0.0, 1.0, 0.0, 1.0, 1.0, -1.0)
+        glMatrixMode(GL_MODELVIEW)  # return to position viewer
+
+    def set_spiral_viz(self):
+        self.viz = 'spiral'
+        self.render_frame = self.render_spiral
+        glMatrixMode(GL_PROJECTION)  # set viewing projection
+        glLoadIdentity()
+        ratio = float(self.width) / self.height
+        glOrtho(-ratio, ratio, -1.0, 1.0, 1.0, -1.0)
+        glMatrixMode(GL_MODELVIEW)  # return to position viewer
+
+    def render_keyboard(self):
         glClear(GL_COLOR_BUFFER_BIT)
         glLoadIdentity()
         with engine_lock:
-            if midi_engine.notes_need_update or time.time() - self.last_update > 0.03:
-                engine.tick()
-                (cx, cy) = midi_engine.get_center()
-                scale = 1.2 / (math.hypot(cx, cy) + 1)
-                (cx, cy) = (scale * cx, scale * cy)
-                note_weights = [0]
-                for note in midi_engine.notes.itervalues():
-                    note.render_age = engine.now - note.start
-                    note.render_decay = math.exp(-note.render_age * 0.5) * note.damper_level
-                    note.render_pos = (note.pitch_coords[0] + cx, note.pitch_coords[1] + cy)
-                    note_weights.append(note.render_decay * note.volume)
-                elapsed = engine.now - self.last_update
-                note_weights.sort()
-                top_2nd_note_weight = note_weights[-2] if len(note_weights) >= 2 else note_weights[-1] * 0.9
-                self.top_2nd_note_weight = max(self.top_2nd_note_weight * math.exp(-elapsed/5.0),
-                                               top_2nd_note_weight, 0.3)
-                self.last_update = engine.now
-                midi_engine.notes_need_update = False
+            self.request_update()
             glBegin(GL_QUADS)
             for (midipitch, note) in midi_engine.notes.items():
                 pitch = midipitch - 21
-                (minx, maxx) = (pitch / 88.0, (pitch + 1) / 88.0)
-                r = math.hypot(*note.render_pos)
-                (f, pc1) = math.modf(math.atan2(note.render_pos[1], note.render_pos[0]) / engine.PC_RADS + 12)
-                pc1 = int(pc1)
-                color = engine.weighted_avg_colors(RGB_COLORS[ pc1  % 12],
-                                                   RGB_COLORS[(pc1+1)%12],
-                                                   1 - f)
-                if r < 1.0:
-                    color = engine.weighted_avg_colors(color, [0.75, 0.75, 0.75], r)
-                # normalize weight to 2nd heaviest note, so top note gets voicing bonus
-                weight = note.render_decay * note.volume / self.top_2nd_note_weight
-                if weight < 1.0:
-                    # decay non-voiced notes slightly faster at attack-time
-                    color = [c * weight * math.exp(weight - 1) for c in color]
-                else:
-                    color = [min(c + (1-c)*(weight-1)/0.4, 1) for c in color]
+                color = self.get_note_color(midipitch, note)
                 glColor3f(*color)
-                glVertex3f(minx, -1., 0.)
-                glVertex3f(maxx, -1., 0.)
-                glVertex3f(maxx,  1., 0.)
-                glVertex3f(minx,  1., 0.)
+                (minx, maxx) = (pitch / 88.0, (pitch + 1) / 88.0)
+                glVertex3f(minx, 0., 0.)
+                glVertex3f(maxx, 0., 0.)
+                glVertex3f(maxx, 1., 0.)
+                glVertex3f(minx, 1., 0.)
             glEnd()
+
+    def render_spiral(self):
+        glClear(GL_COLOR_BUFFER_BIT)
+        glLoadIdentity()
+        with engine_lock:
+            self.request_update()
+            glBegin(GL_QUADS)
+            glColor3f(.1, .1, .1)
+            for pitch in xrange(88):
+                self.draw_spiral_pitch(pitch)
+            for (midipitch, note) in midi_engine.notes.items():
+                pitch = midipitch - 21
+                color = self.get_note_color(midipitch, note)
+                glColor3f(*color)
+                self.draw_spiral_pitch(pitch)
+            glEnd()
+
+    def draw_spiral_pitch(self, pitch):
+        theta = 2*math.pi * 5.03/12 * pitch
+        rad = 0.97 ** pitch * 1.15 / (pitch/88.0 + 1)
+        (x, y) = (rad * math.cos(theta), rad * math.sin(theta))
+        rad *= 0.15
+        glVertex3f(x - rad, y - rad, 0.)
+        glVertex3f(x + rad, y - rad, 0.)
+        glVertex3f(x + rad, y + rad, 0.)
+        glVertex3f(x - rad, y + rad, 0.)
+
+    def request_update(self):
+        if midi_engine.notes_need_update or time.time() - self.last_update > 0.03:
+            engine.tick()
+            (cx, cy) = midi_engine.get_center()
+            scale = 1.2 / (math.hypot(cx, cy) + 1)
+            (cx, cy) = (scale * cx, scale * cy)
+            for note in midi_engine.notes.itervalues():
+                note.render_age = engine.now - note.start
+                note.render_decay = math.exp(-note.render_age * 0.5) * note.damper_level
+                note.render_pos = (note.pitch_coords[0] + cx, note.pitch_coords[1] + cy)
+            elapsed = engine.now - self.last_update
+            self.last_update = engine.now
+            midi_engine.notes_need_update = False
+            # now find second weightiest note
+            note_weights = sorted(n.render_decay * n.volume for n in midi_engine.notes.itervalues())
+            top_2nd_note_weight = (note_weights[-2:] + [0])[0]
+            self.top_2nd_note_weight = max(self.top_2nd_note_weight * math.exp(-elapsed/5.0),
+                                           top_2nd_note_weight, 0.3)
+
+    def get_note_color(self, midipitch, note):
+        r = math.hypot(*note.render_pos)
+        (f, pc1) = math.modf(math.atan2(note.render_pos[1], note.render_pos[0]) / engine.PC_RADS + 12)
+        pc1 = int(pc1)
+        color = engine.weighted_avg_colors(RGB_COLORS[ pc1  % 12],
+                                           RGB_COLORS[(pc1+1)%12],
+                                           1 - f)
+        if r < 1.0:
+            color = engine.weighted_avg_colors(color, [0.75, 0.75, 0.75], r)
+        # normalize weight to 2nd heaviest note, so top note gets voicing bonus
+        weight = note.render_decay * note.volume / self.top_2nd_note_weight
+        if weight < 1.0:
+            # decay non-voiced notes slightly faster at attack-time
+            color = [c * weight * math.exp(weight - 1) for c in color]
+        else:
+            color = [min(c + (1-c)*(weight-1)/0.4, 1) for c in color]
+        return color
+
 
 midi_engine = engine.Engine()
 def midi_cb(code, *args):
@@ -115,9 +161,10 @@ def main(args):
     except glfw_app.GlfwError as e:
         logging.error(e.message)
         return
-    init_gl(width, height)
+    renderer = Renderer(width, height)
+    renderer.set_spiral_viz()
     logging.info("Entering render loop")
-    app.run(Renderer().render_frame)
+    app.run(renderer.render_frame)
 
 
 if __name__ == '__main__':
