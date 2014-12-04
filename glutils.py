f1e6b849055dd06912f5e8ef6c543a9eca1f33c9
nanotone <nanotone@gmail.com>
2014-12-04 18:35:59 -0500

    Add gl scaled() context manager

--- glutils.py
+++ glutils.py
@@ -3,9 +3,17 @@ import contextlib
 from OpenGL.GL import *
 
 @contextlib.contextmanager
-def translated(x, y, z):
+def translated(x=0, y=0, z=0):
     glPushMatrix()
     glTranslatef(x, y, z)
     yield
     glPopMatrix()
 
+@contextlib.contextmanager
+def scaled(x, y=None, z=None):
+    if y is None: y = x
+    if z is None: z = y
+    glPushMatrix()
+    glScalef(x, y, z)
+    yield
+    glPopMatrix()
